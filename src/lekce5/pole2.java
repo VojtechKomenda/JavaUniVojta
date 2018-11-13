b0526a05984834e3396ea0788cb68b0fd9987d24
Vojtech <Vojtech@10.3.60.247>
2018-11-13 14:28:55 +0100

    test

---
+++ src/lekce5/pole2.java
@@ -0,0 +1,22 @@
+package lekce5;
+
+public class pole2 {
+
+	int mojePrvniPole[] =new int[10];
+	
+	
+void setting () {
+	mojePrvniPole[0]=25;
+	System.out.println(mojePrvniPole[0]);
+	
+	 mojePrvniPole= {25,18,45,86,4,9,450,18,87,63};
+	 
+
+}
+	
+	
+	
+			
+	
+	
+}
